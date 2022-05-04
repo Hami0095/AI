4dec3748dbc4895f414e7c406d1086b30cf131f8
Mohammad Abdur Rehman Cheema <59124007+Hami0095@users.noreply.github.com>
2022-05-04 13:58:01 +0500

    Update churn_modelling.py

--- churn_modelling.py
+++ churn_modelling.py
@@ -129,9 +129,9 @@ print(ann.predict(sc.transform([[1,0,0, 150, 0, 35, 1, 500,0,0,1,5000]]))>0.5)
 
 """**Solution**
 
-Therefore, our ANN model predicts that this customer stays in the bank!
+##Therefore, our ANN model predicts that this customer stays in the bank!
 
-**Important note 1:** the values of the features were all input in a double pair of square brackets. That's because the "predict" method always expects a 2D array as the format of its inputs. And putting our values into a double pair of square brackets makes the input exactly a 2D array.
+##**Important note 1:** the values of the features were all input in a double pair of square brackets. That's because the "predict" method always expects a 2D array as the format of its inputs. And putting our values into a double pair of square brackets makes the input exactly a 2D array.
 
-**Important note 2:** Notice also that the "France" country was not input as a string in the last column but as "1, 0, 0" in the first three columns. That's because of course the predict method expects the one-hot-encoded values of the state, and as we see in the first row of the matrix of features X, "France" was encoded as "1, 0, 0". And be careful to include these values in the first three columns, because the dummy variables are always created in the first columns.
+##**Important note 2:** Notice also that the "France" country was not input as a string in the last column but as "1, 0, 0" in the first three columns. That's because of course the predict method expects the one-hot-encoded values of the state, and as we see in the first row of the matrix of features X, "France" was encoded as "1, 0, 0". And be careful to include these values in the first three columns, because the dummy variables are always created in the first columns.
 
